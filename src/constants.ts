a62d3a3e4377b271d45b422002c83db3cc32cf33
Xingchen Hong <zodiase@gmail.com>
2021-07-10 01:05:53 -0700

    Restrict chunk size to 300MB.

--- src/constants.ts
+++ src/constants.ts
@@ -19,3 +19,14 @@ export const WorkFilePermission = 0o600;
  * Permission needed on the working files. Used by `fs.access`.
  */
 export const WorkFileAccessMode = FsConst.F_OK | FsConst.R_OK | FsConst.W_OK;
+
+/**
+ * Maximum chunk size in MBs.
+ * @see https://github.com/Zodiase/try-storage-cli/issues/5.
+ */
+export const MaxChunkSize = 300;
+
+/**
+ * How many bytes in one MB.
+ */
+export const MegaBytes = 1000000;
